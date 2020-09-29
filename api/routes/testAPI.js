7988e6f47a370497510a50b244a63b77b61a17a1
Adam Feather <afeather20@gmail.com>
2020-09-29 13:37:46 -0700

    First commit of program

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,130 @@
-var express = require("express");
-var router = express.Router();
+const neatCsv = require('neat-csv');
+let express = require("express");
+let router = express.Router();
+let https = require('https');
+let ipvigilante = {
+    host: 'ipvigilante.com',
+    port : 443,
+    method : 'GET',
+    headers: {'User-Agent': 'request'}
+};
+//We create a index of countries and their indexes (cacheing for now in an ideal world we would put this into a database and just be able to query)
+let countryIndexLatestDate = {};
 
-router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+//File reading csv file into a caches item on the server so we can read from it easily
+const fs = require('fs');
+let csvData;
+fs.readFile('./data/bigMac.csv', async (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    csvData = await neatCsv(data);
 });
+
+
+//We only need a get request to get our country of origin and our 
+router.get("/", async function(req, response, next) {
+
+        //Initial check gets your ip address 
+        await https.get(ipvigilante, function (res){ 
+
+            var json = '';
+     
+            res.on('data', function (chunk) {
+                
+                json += chunk;
+                
+            });
+            res.on('end', function () {
+                if (res.statusCode === 200) {
+                    try {
+                       
+                        let result = JSON.parse(json);
+                        let userData = findCountry(result.data.country_name);
+                        let RandomCountryIndex = csvData[randomInteger(0, csvData.length)].Country;
+                        let RandomCountry = findCountry(RandomCountryIndex, userData.Country);
+                        let clientRenderedData = {
+                            UserData: userData,
+                            RandomCountry: RandomCountry,
+                            conversionRate: getConverstionRate(userData, RandomCountry)
+                        }
+                        response.send(clientRenderedData);
+                    } catch (e) {
+                        console.log("__________________________________________________")
+                        console.log(e);
+                        console.log('Error parsing JSON!');
+                    }
+                } else {
+                    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                    console.log('Status:', res.statusCode);
+                }
+            });
+        
+        
+        }).on('error', function(err){ 
+            console.log(err);
+            response.send("Could not resolve country name");
+    
+        });
+
+   
+
+});
+
+router.get("/getRandomCountry", async function(req, response, next) {
+
+    let userData = findCountry(req.query.UserCountry);
+    let RandomCountryIndex = csvData[randomInteger(0, csvData.length)].Country;
+    let RandomCountry = findCountry(RandomCountryIndex, userData.Country);
+    let clientRenderedData = {
+        
+        UserData: userData,
+        RandomCountry: RandomCountry,
+        conversionRate: getConverstionRate(userData, RandomCountry)
+    }
+    response.send(clientRenderedData);
+
+})
+
+function findCountry(countryName, userCountry) { 
+    if(countryName != userCountry){ 
+        if(countryIndexLatestDate[countryName] ){ 
+      
+            return csvData[countryIndexLatestDate[countryName]]
+            
+        }
+        else{ 
+            let maxDateCountry = 0;
+            let countryIndex = 0;
+            let clientRenderData = {};
+            for(let i = 0; i < csvData.length; i++) { 
+                if(csvData[i].Country == countryName &&  (maxDateCountry < csvData[i].Date || maxDateCountry == 0)) { 
+                    maxDateCountry = csvData[i].Date;
+                    countryIndex = i;
+                    clientRenderData = csvData[i]
+    
+                }
+            }
+            countryIndexLatestDate[clientRenderData.Country] = countryIndex;
+            return clientRenderData;
+        }
+    }
+    else { 
+        return findCountry(csvData[randomInteger(0, csvData.length)].Country,userCountry)
+    }
+ 
+}
+
+function randomInteger(min, max) {
+    
+    let rando =  Math.floor(Math.random() * (max - min + 1)) + min;
+    return rando;
+  }
+
+  function getConverstionRate(userData, RandomCountry){ 
+
+        return userData['Dollar price'] / RandomCountry['Dollar price'];
+  }
 
 module.exports = router;
--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,134 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = { 
+            UserCountry: "",
+            UserCurrencyPerBigMac: 0,
+            UserPPP: 0,
+            UserBigMacsTheyCanBuy: 0,
+            UserMoney: 0,
+            UserDollarPrice:  0,            
+            RandomCountry:0,
+            RandomCountryNumOfBigMacs:0,
+            RandomCountryLocalPrice: 0,
+            RandomCountryMoneyAmount: 0,
+            RandomCountryPPP: 0,
+            ConversionRate: 0
+
+        };
     }
 
-    callAPI() {
+
+
+    //Initial get to gather the ip location of the user and get the data for to populate the compoenets
+    initialGetOfIpAndRandomCountry() {
         fetch("http://localhost:9000/testAPI")
             .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }))
+            .then(res =>
+                this.setState({ 
+                    UserCountry: JSON.parse(res).UserData.Country,
+                    UserCurrencyPerBigMac:  JSON.parse(res).UserData['Local price'],
+                    UserDollarPrice:  JSON.parse(res).UserData['Dollar price'],
+                    UserPPP:  JSON.parse(res).UserData['Dollar PPP'],
+                    RandomCountry: JSON.parse(res).RandomCountry.Country,
+                    RandomCountryLocalPrice: JSON.parse(res).RandomCountry['Local price'],
+                    RandomCountryPPP: JSON.parse(res).RandomCountry['Dollar PPP'],
+                    RandomDollarPrice:  JSON.parse(res).RandomCountry['Dollar price'],
+                    ConversionRate: JSON.parse(res).conversionRate
+                   })
+                 )
+            .then(res =>{
+                localStorage.setItem('UserCountry',this.state.UserCountry);
+            })
             .catch(err => err);
+
+    }
+
+
+    //Get a new random country by clicking the button so we dont call the ip api every call
+    getNewRandomCountry(){ 
+        fetch("http://localhost:9000/testAPI/getRandomCountry?UserCountry="+localStorage.getItem('UserCountry')+"")
+        .then(res => res.text())
+        .then(res => {
+            try{ 
+                this.setState({ 
+
+                    RandomCountry: JSON.parse(res).RandomCountry.Country,
+                    RandomCountryLocalPrice: JSON.parse(res).RandomCountry['Local price'],
+                    RandomCountryPPP: JSON.parse(res).RandomCountry['Dollar PPP'],
+                    RandomDollarPrice:  JSON.parse(res).RandomCountry['Dollar price'],
+                    ConversionRate: JSON.parse(res).conversionRate
+                   });
+
+                   let numOfMacsBought = this.state.UserMoney / this.state.UserCurrencyPerBigMac;
+                   let conversion = this.state.ConversionRate;
+                   let numOfRandomMacs = parseInt(numOfMacsBought * conversion);
+                   let randomMoneyAmount = conversion * this.state.UserMoney;
+                   this.setState( { 
+                       UserBigMacsTheyCanBuy: parseInt(numOfMacsBought),
+                       RandomCountryNumOfBigMacs: numOfRandomMacs,
+                       RandomCountryMoneyAmount: randomMoneyAmount
+           
+                   })
+            }
+            catch(err) { 
+                console.log(err);
+            }
+            
+        })   
+        .then(res =>{
+            console.log(this.state);
+            localStorage.setItem('UserCountry',this.state.UserCountry);
+        })
+        .catch(err => err);
+
+       
+    }
+
+    //Take money input and update componenets according
+    handelMoneyInput(e) { 
+        let numOfMacsBought = e.target.value / this.state.UserCurrencyPerBigMac;
+        let conversion = this.state.ConversionRate;
+        let numOfRandomMacs = parseInt(numOfMacsBought * conversion);
+        let randomMoneyAmount = conversion * e.target.value;
+        this.setState( { 
+            UserMoney: e.target.value,
+            UserBigMacsTheyCanBuy: parseInt(numOfMacsBought),
+            RandomCountryNumOfBigMacs: numOfRandomMacs,
+            RandomCountryMoneyAmount: randomMoneyAmount
+
+        })
     }
 
     componentDidMount() {
-        this.callAPI();
+
+
+        this.initialGetOfIpAndRandomCountry();
+       
     }
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
+            <br/>
+                <h3>You are in {this.state.UserCountry}</h3>
+                <div>Please Enter an amount of money in your local currency</div>
+                <input value={this.state.userMoney} onChange={(e) => this.handelMoneyInput(e)} />
+                <br/>
+                <br/>
+                <div>You could buy {this.state.UserBigMacsTheyCanBuy} Big Macs in your country</div>
+                <div>Your Dollar Purchasing Parity (PPP) is {this.state.UserPPP}</div>
+                <div>This is a simple lookup to the table</div>
+                <br/>
+                <button onClick={(e) => this.getNewRandomCountry()}>Change Random Country</button>
+                <br/>
+                <h3>Random Country: {this.state.RandomCountry}</h3>
+                <div>You could buy {this.state.RandomCountryNumOfBigMacs} of Big Macs in {this.state.RandomCountry} with {this.state.UserMoney}</div>
+                <div>Your {this.state.UserMoney} is worth about {this.state.RandomCountryMoneyAmount} in {this.state.RandomCountry}</div>
             </div>
         );
     }
